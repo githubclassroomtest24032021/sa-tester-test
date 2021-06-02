4bafb4cac62d4487be17988882455d693ecdea46
En Rong <No public email provided>
2021-06-02 20:34:15 +0800

    Changes made from Source Academy

---
+++ Q2/SavedCode.js
@@ -0,0 +1,13 @@
+// Task 2
+function partition(xs, p) {
+    // Your answer from Task 1
+}
+
+function quicksort(xs) {
+    // Your answer here
+}
+
+// Test
+// const my_list = list(23, 12, 56, 92, -2, 0);
+// quicksort(my_list);
+ 
