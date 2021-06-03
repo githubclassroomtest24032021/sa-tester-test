b49a29cc619c451afb753f78396cfa454d5753e4
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-06-03 12:26:30 +0800

    Update StarterCode.js

--- Q4/StarterCode.js
+++ Q4/StarterCode.js
@@ -1,13 +1,13 @@
 MCQ
 {
-  questions:
+  "questions":
   [
-    { solution: "Θ(1)", hint:"" },
-    { solution: "Θ(log _n_)", hint:"" },
-    { solution: "Θ(_n_)", hint:"" },
-    { solution: "Θ(_n_ log _n_)", hint:"" },
-    { solution: "Θ(_n_²)", hint:"" },
-    { solution: "Θ(_n_³)", hint:"" },
+    { "solution": "Θ(1)", "hint":"" },
+    { "solution": "Θ(log _n_)", "hint":"" },
+    { "solution": "Θ(_n_)", "hint":"" },
+    { "solution": "Θ(_n_ log _n_)", "hint":"" },
+    { "solution": "Θ(_n_²)", "hint":"" },
+    { "solution": "Θ(_n_³)", "hint":"" },
   ],
-  answer: -1
+  "answer": -1
 }
