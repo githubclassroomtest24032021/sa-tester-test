410fc62b32f9f5a5be2118407ceae0937d8a9f91
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-06-10 12:38:59 +0800

    Update StarterCode.js

--- Q3/StarterCode.js
+++ Q3/StarterCode.js
@@ -1,12 +1,12 @@
 MCQ
 {
-  "options":
+  "choices":
   [
-    { "choice": "Θ(1)", "hint":"" },
-    { "choice": "Θ(log _n_)", "hint":"" },
-    { "choice": "Θ(_n_)", "hint":"" },
-    { "choice": "Θ(_n_ log _n_)", "hint":"" },
-    { "choice": "Θ(_n_²)", "hint":"" }
+    { "option": "Θ(1)", "hint":"" },
+    { "option": "Θ(log _n_)", "hint":"" },
+    { "option": "Θ(_n_)", "hint":"" },
+    { "option": "Θ(_n_ log _n_)", "hint":"" },
+    { "option": "Θ(_n_²)", "hint":"" }
   ],
   "answer": -1,
   "solution": 2
