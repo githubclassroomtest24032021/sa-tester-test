2495fac06c2012b247549d3acc4cad5461fabb59
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-06-10 12:39:42 +0800

    Update StarterCode.js

--- Q5/StarterCode.js
+++ Q5/StarterCode.js
@@ -1,13 +1,13 @@
 MCQ
 {
-  "options":
+  "choices":
   [
-    { "choice": "Θ(1)", "hint":"" },
-    { "choice": "Θ(log _n_)", "hint":"" },
-    { "choice": "Θ(_n_)", "hint":"" },
-    { "choice": "Θ(_n_ log _n_)", "hint":"" },
-    { "choice": "Θ(_n_²)", "hint":"" },
-    { "choice": "Θ(_n_³)", "hint":"" }
+    { "option": "Θ(1)", "hint":"" },
+    { "option": "Θ(log _n_)", "hint":"" },
+    { "option": "Θ(_n_)", "hint":"" },
+    { "option": "Θ(_n_ log _n_)", "hint":"" },
+    { "option": "Θ(_n_²)", "hint":"" },
+    { "option": "Θ(_n_³)", "hint":"" }
   ],
   "answer": -1
 }
